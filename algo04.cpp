97b52884072e6230f7ddcc4f0dce3c3bf90e12a9
Basti Destruction <basti@dest.cc>
2018-11-15 11:26:48 +0100

    finished implementation of base functions

--- algo04.cpp
+++ algo04.cpp
@@ -1,16 +1,54 @@
 #include "binary_tree.hpp"
+#include <vector>
 
-int main() {
-  binary_tree::Tree<int> tree{};
-  tree.insert(12);
-  tree.insert(7);
-  tree.insert(18);
-  tree.insert(2);
-  tree.insert(10);
-  tree.insert(14);
-  tree.insert(23);
-  tree.insert(8);
-  tree.insert(9);
-  tree.print();
+
+int main(int argc, char* argv[]) {
+  if(argc < 2) {
+    std::cout << "Usage: algo04 \"[<numer1>[<number2> ...]]\"" << std::endl;
+    return 2;
+  }
+
+  if(std::string{argv[1]} == std::string{"test"}) {
+    binary_tree::Tree<int> tree{};
+    tree.insert(12);
+    tree.insert(7);
+    tree.insert(18);
+    tree.insert(2);
+    tree.insert(10);
+    tree.insert(14);
+    tree.insert(23);
+    tree.insert(8);
+    tree.insert(9);
+    tree.print();
+    tree.remove(12);
+    tree.print();
+    std::cout << tree.findMin()->getValue() << std::endl;
+    std::cout << tree.find(23)->getValue() << std::endl;
+    return 0;
+  }
+
+  std::vector<int> inputs;
+  for(size_t i = 1; i < static_cast<size_t>(argc); i++) {
+    try {
+      inputs.push_back(std::stoi(argv[i]));
+    } catch(const std::exception& e) {
+      std::cerr << "invalid argument \"" << argv[i] << "\": unable to convert to integer" << std::endl;
+      return 3;
+    }
+  }
+
+  if(inputs.size() <= 0) {
+    std::cerr << "at least one numer has to be supplied" << std::endl;
+    return 4;
+  }
+
+  binary_tree::Tree<int> tree;
+
+  for(size_t i = 0; i < inputs.size(); i++) {
+    tree.insert(inputs[i]);
+  }
+
+  tree.printTheNodesOrNone();
+  
   return 0;
 }
--- binary_tree.hpp
+++ binary_tree.hpp
@@ -31,6 +31,27 @@ namespace binary_tree {
             }
         }
 
+        static bool printTheNodesOrNone(const std::shared_ptr<Node<T>>& node) {
+            bool found = false;
+            if((node->m_value % 5) == 0) {
+                if(node->m_pLeft) {
+                    found = true;
+                    std::cout << node->m_pLeft->getValue() << " ";
+                }
+                if(node->m_pRight) {
+                    found = true;
+                    std::cout << node->m_pRight->getValue() << " ";
+                }
+            }
+            if(node->m_pLeft){
+                found |= Node<T>::printTheNodesOrNone(node->m_pLeft);
+            }
+            if(node->m_pRight){
+                found |= Node<T>::printTheNodesOrNone(node->m_pRight);
+            }
+            return found;
+        }
+
         void insert(const T value) {
             if(value < m_value) {
                 if(m_pLeft) {
@@ -46,11 +67,83 @@ namespace binary_tree {
                 }
             }
         }
-        bool remove(const T value, std::shared_ptr<Node<T>>& node) {
-            
+
+        static void findMin(std::shared_ptr<Node<T>>& node) {
+            if(!node->m_pLeft) {
+                return;
+            }
+            node = node->m_pLeft;
+            findMin(node);
         }
-        const std::shared_ptr<Node<T>> find(const T value) const;
-    private:
+
+        static void findMax(std::shared_ptr<Node<T>>& node) {
+            if(!node->m_pRight) {
+                return;
+            }
+            node = node->m_pRight;
+            findMax(node);
+        }
+
+        static bool remove(const T value, std::shared_ptr<Node<T>>& node) {
+            if(value < node->getValue()) {
+                if(!node->m_pLeft) {
+                    return false;
+                }
+                return remove(value, node->m_pLeft);
+            }
+            if(value > node->getValue()) {
+                if(!node->m_pRight) {
+                    return false;
+                }
+                return remove(value, node->m_pRight);
+            }
+
+            // node found, remove this
+            if(!node->m_pLeft && !node->m_pRight) {
+                node = std::shared_ptr<Node<T>>(nullptr);
+                return true;
+            }
+
+            if(!node->m_pLeft) {
+                node = node->m_pRight;
+                return true;
+            }
+
+            if(!node->m_pRight) {
+                node = node->m_pLeft;
+                return true;
+            }
+
+            std::shared_ptr<Node<T>> nextBiggest = node->m_pRight;
+            Node<T>::findMin(nextBiggest);
+            Node<T>::remove(nextBiggest->getValue(), node);
+            nextBiggest->m_pLeft = node->m_pLeft;
+            nextBiggest->m_pRight = node->m_pRight;
+            node = nextBiggest;
+            return true;
+        }
+
+        static bool find(const T value, std::shared_ptr<Node<T>>& node) {
+            if(value == node->m_value) {
+                return true;
+            }
+            if(value < node->m_value) {
+                if(!node->m_pLeft) {
+                    node = nullptr;
+                    return false;
+                }
+                node = node->m_pLeft;
+                return Node<T>::find(value, node);
+            }
+
+            if(!node->m_pRight) {
+                node = nullptr;
+                return false;
+            }
+            node = node->m_pRight;
+            return Node<T>::find(value, node);
+        }
+
         const T m_value;
         std::shared_ptr<Node<T>> m_pLeft;
         std::shared_ptr<Node<T>> m_pRight;
@@ -68,19 +161,44 @@ namespace binary_tree {
                 m_pRoot->print();
             }
         }
-        const std::shared_ptr<Node<T>>& find(const T value) const {
+
+        void printTheNodesOrNone() const {
+            if(!m_pRoot || !Node<T>::printTheNodesOrNone(m_pRoot)) {
+                std::cout << "None";
+            }
+            std::cout << std::endl;
+        }
+
+        const std::shared_ptr<Node<T>> find(const T value) const {
             if(!m_pRoot) {
                 return nullptr;
             }
 
-            if(m_pRoot->getValue() == value) {
-                return m_pRoot;
+            auto ret = m_pRoot;
+            Node<T>::find(value, ret);
+            return ret;
+        }
+
+        const std::shared_ptr<Node<T>> findMin() const {
+            if(!m_pRoot) {
+                return nullptr;
             }
 
-            return m_pRoot->find(value);
+            auto newPtr = m_pRoot;
+            Node<T>::findMin(newPtr);
+            return newPtr;
         }
-        const std::shared_ptr<Node<T>>& findMin() const;
-        const std::shared_ptr<Node<T>>& findMax() const;
+
+        const std::shared_ptr<Node<T>> findMax() const {
+            if(!m_pRoot) {
+                return nullptr;
+            }
+
+            auto newPtr = m_pRoot;
+            Node<T>::findMax(newPtr);
+            return newPtr;
+        }
+
         void insert(const T value) {
             if(m_pRoot) {
                 m_pRoot->insert(value);
@@ -88,11 +206,12 @@ namespace binary_tree {
                 m_pRoot = std::make_shared<Node<T>>(value);
             }
         }
+
         bool remove(const T value) {
             if(!m_pRoot) {
                 return false;
             }
-            
+            return Node<T>::remove(value, m_pRoot);
         }
     private:
         std::shared_ptr<Node<T>> m_pRoot;
